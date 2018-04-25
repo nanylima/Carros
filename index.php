d9a3ed32f03b97eb06ac3f8cbf28e813719a378e
Daniela <nanylima2010@gmail.com>
2018-04-25 19:02:35 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang=“pt-br”>
+	<head>
+		<title>Veículos</title>
+		<meta charset=“utf-8”/>
+		<link rel="stylesheet" href="estilos.css"/> 
+	</head>
+	<body>
+	
+		<header>
+			<?php
+			include "topo.php";
+			?>
+		</header>
+		
+
+		<section id="slider">
+			<?php
+			include "slider.html";
+			?>
+		</section>
+		
+	</body>
+</html> 
---
+++ topo.php
@@ -0,0 +1,8 @@
+<section id='logo_topo'></section> 
+	<nav id='menu'>
+		<a class='btmenu' href='index.php'  target='_self'>HOME</a>
+		<a class='btmenu' href='index.php'  target='_self'>CARRO</a>
+		<a class='btmenu' href='index.php'  target='_self'>CONTATO</a>
+		<a class='btmenu' href='index.php'  target='_self'>LOCALIZAÇÃO</a>
+
+	</nav> 
